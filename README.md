9b6706ff663fe70ee09e602e65e9e9e625ca45be
2betop <liaoxuezhi@icloud.com>
2015-11-20 23:58:30 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+fis3-preprocessor-css2js
+===============
+讲 css 解析成 js 文件，用 js 的方式去加载。
+
+只支持 fis3
+
+```
+npm install -g fis3-preprocessor-css2js
+```
+
+使用方式配置如下：
+
+```
+fis.match('/modules/css/*.{scss,css,less}', {
+  preprocessor: fis.plugin('css2js'),
+  rExt: '.js',
+  isMod: true
+})
+```
+
+配置完后，可以在 js 中用 js loader 引用 css 了，如： `require(`css/xxx.css`)` 
---
+++ index.js
@@ -0,0 +1,78 @@
+var tinytim = require('tinytim');
+
+module.exports = function(content, file, conf) {
+  // 先走一次 css 处理，
+  // 如果要在 css 的流程中添加内容，请使用
+  // fis.match('*.css:css')
+  cssContent = fis.compile.partial(content, file, {
+    ext: '.css',
+    isCssLike: true
+  });
+
+  cssContent = cssContent.replace(/[\n|\r]/g, " ");
+  cssContent = cssContent.replace(/'/g, '\"');
+  cssContent = "'" + cssContent + "'";
+
+  var injectCssFn = tinytim.render(conf.templates.css_injector, {});
+  content = tinytim.render(conf.templates[conf.template || 'requirejs_runner'], {
+    cssContent: cssContent, 
+    injectCssFn: injectCssFn
+  });
+
+  return content;
+};
+
+const CSS_INJECTOR = "(function (css) {\n" +
+"    var headEl = document.getElementsByTagName('head')[0];\n" +
+"    var styleEl = document.createElement('style');\n" +
+"    headEl.appendChild(styleEl);\n" +
+"    \n" +
+"    if (styleEl.styleSheet) {\n" +
+"        if (!styleEl.styleSheet.disabled) {\n" +
+"            styleEl.styleSheet.cssText = css;\n" +
+"        }\n" +
+"    } else {\n" +
+"        try {\n" +
+"            styleEl.innerHTML = css\n" +
+"        } catch(e) {\n" +
+"            styleEl.innerText = css;\n" +
+"        }\n" +
+"    }\n" +
+"})";
+
+const REQUREJS_INJECT = "define([], function() {\n" +
+"    var cssContent = {{ cssContent }};\n" +
+"    var injectCssFn = {{ injectCssFn }};\n" +
+"\n" +
+"    return {\n" +
+"        inject: function() {\n" +
+"            injectCssFn(cssContent);\n" +
+"        }\n" +
+"    };\n" +
+"});";
+
+const REQUREJS_RUNNER = "define([], function() {\n" +
+"    var cssContent = {{ cssContent }};\n" +
+"    var injectCssFn = {{ injectCssFn }};\n" +
+"\n" +
+"    injectCssFn(cssContent);\n" +
+"\n" +
+"    return {};\n" +
+"});";
+
+const VANILLA_RUNNER = "(function() {\n" +
+"    var cssContent = {{ cssContent }};\n" +
+"    var injectCssFn = {{ injectCssFn }};\n" +
+"\n" +
+"    injectCssFn(cssContent);\n" +
+"})();";
+
+module.exports.defaultOptions = {
+  templates: {
+    css_injector: CSS_INJECTOR,
+    requirejs_inject: REQUREJS_INJECT,
+    requirejs_runner: REQUREJS_RUNNER,
+    vanilla_runner: VANILLA_RUNNER
+  },
+  template: 'vanilla_runner'
+}
